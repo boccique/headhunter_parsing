01c245bdc318beff1de4bf20d81b369bb2d5654c
John Yakovl <tchuwipeach@yandex.com>
2023-09-21 18:22:12 +0700

    4124

--- main.py
+++ main.py
@@ -5,13 +5,30 @@ from bs4 import BeautifulSoup
 import time
 import json
 
+
 def get_links(text):
     ua = fake_useragent.UserAgent()
     data = requests.get(
         url=f"https://hh.ru/search/resume?isDefaultArea=true&exp_period=all_time&logic=normal&pos=full_text&search_period=0&order_by=relevance&filter_exp_period=all_time&relocation=living_or_relocation&text={text}&gender=unknown&page=1",
         headers={"user-agent": ua.random}
     )
-    print(data.content)
+    if data.status_code != 200:
+        return
+    soup = BeautifulSoup(data.content, "lxml")
+    try:
+        page_count = int(
+            soup.find("div", attrs={"class": "pager"}).find_all("span", recursive=False)[-1].find("a").find(
+                "span").text())
+    except:
+        return
+    for page in range(page_count):
+        data = requests.get(
+            url=f"https://hh.ru/search/resume?isDefaultArea=true&exp_period=all_time&logic=normal&pos=full_text&search_period=0&order_by=relevance&filter_exp_period=all_time&relocation=living_or_relocation&text={text}&gender=unknown&page={page}",
+            headers={"user-agent": ua.random}
+        )
+
+
+
 
 
 def get_resume(link):
